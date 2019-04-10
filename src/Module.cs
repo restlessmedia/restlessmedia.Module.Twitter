0379670c6b960b0e235145cdff8daaf32cf89106
restlessmedia <phillycooper@hotmail.com>
2019-04-10 17:18:22 +0100

    Added check for bitly settings to allow null if not being used.

--- src/Module.cs
+++ src/Module.cs
@@ -8,9 +8,17 @@ namespace restlessmedia.Module.Twitter
     public void RegisterComponents(ContainerBuilder containerBuilder)
     {
       containerBuilder.RegisterSettings<IBitlySettings>("restlessmedia/bitly", required: false);
-      containerBuilder.RegisterType<BirdFeedProvider>().As<ITwitterProvider>().SingleInstance(); 
+      containerBuilder.RegisterType<BirdFeedProvider>().As<ITwitterProvider>().SingleInstance();
       containerBuilder.RegisterType<BitlyProvider>().As<IBitlyProvider>().SingleInstance();
-      containerBuilder.RegisterType<BitlyService>().As<IBitlyService>().SingleInstance();
+      containerBuilder.Register(x =>
+      {
+        if (x.TryResolve(out IBitlySettings bitlySettings))
+        {
+          return new BitlyService(bitlySettings, x.Resolve<IBitlyProvider>());
+        }
+
+        return null;
+      }).As<IBitlyService>().SingleInstance();
       containerBuilder.RegisterType<TwitterService>().As<ITwitterService>().SingleInstance();
     }
   }
